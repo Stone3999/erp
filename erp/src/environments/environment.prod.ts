692fc963ec03bf1cee9b79b04b5c6988e09cb944
David Quiroz <98960850+Stone3999@users.noreply.github.com>
2026-04-12 21:04:20 -0600

    ERP Rooms4ums - Prueba 1: URL de Producción Render Configurada

--- erp/src/environments/environment.prod.ts
+++ erp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://erp-api-gateway.onrender.com' // CAMBIA ESTO por la URL real que te dé Render
+  apiUrl: 'https://erp-all-in-one-backend.onrender.com'
 };
